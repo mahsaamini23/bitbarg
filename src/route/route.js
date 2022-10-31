3d0a61b4e5616a10c9f57f9400a48959a368ec5d
ms.moradi23 <ms.moradi23@yahoo.com>
2022-10-31 11:18:41 +0330

    add Layout to router

--- src/route/route.js
+++ src/route/route.js
@@ -1,10 +1,17 @@
 import {createBrowserRouter} from "react-router-dom";
 import Home from "page/Home";
+import Layout from "component/Layout/Layout";
 
 const router = createBrowserRouter([
     {
         path:"/",
-        element:<Home/>
+        element:<Layout/>,
+        children:[
+            {
+                index:true,
+                element:<Home/>
+            },
+        ]
     }
 ])
 
