53be867e2e7a296f9c83e446241806f1a371b72c
ms.moradi23 <ms.moradi23@yahoo.com>
2022-10-31 11:00:01 +0330

    Syntax correction

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import './App.css';
 import Home from "./page/Home";
-import {BrowserRouter} from "react-router-dom";
+import {RouterProvider} from "react-router-dom";
 import router from "./route/route";
 
 function App() {
   return (
-    <BrowserRouter  router={router}>
-      <Home/>
-    </BrowserRouter>
+    <RouterProvider router={router}/>
   );
 }
 
