07df0e13d0796e5935be709f16623fb398e350b8
ms.moradi23 <ms.moradi23@yahoo.com>
2022-11-08 23:17:02 +0330

    modify style

--- src/component/Table/Table.jsx
+++ src/component/Table/Table.jsx
@@ -12,10 +12,13 @@ import InsertChartOutlinedIcon from '@mui/icons-material/InsertChartOutlined';
 import Typography from '@mui/material/Typography';
 import TrendingDownIcon from '@mui/icons-material/TrendingDown';
 import TrendingUpIcon from '@mui/icons-material/TrendingUp';
+import Card from '../Card/Card';
+import Button from '@mui/material/Button';
 
 const MainTable=()=>{
 
     const [coins , setCoins] = useState([])
+    const [state , setState] = useState("maximum")
 
     const handleGetData = async () => {
         const data = await handleGetDataCoins()
@@ -26,6 +29,18 @@ const MainTable=()=>{
     useEffect(()=>{
         handleGetData()
     },[])
+
+    const handleMaximum =()=>{
+        setState("maximum")
+    }
+
+    const handleMinimum =()=>{
+        setState("minimum")
+    }
+
+    const maxValue = coins.map(coin=>coin.price_change_percentage_24h>0 ? coin : '');
+    const minValue = coins.map(coin=>coin.price_change_percentage_24h<0 ? coin : '');
+
 
     return(
     <div>
@@ -40,26 +55,26 @@ const MainTable=()=>{
                 </Grid>
             </Grid>
             <Grid sx={{margin:'30px 0'}}>
-                <Grid sx={{width:'35%',height:'43px', borderRadius:'5px', backgroundColor:'#fafafa', display:'flex', flexDirection:'row'}}>
+                <Grid sx={{width:'35%',height:'43px', borderRadius:'5px', backgroundColor:'#e0e0e0', display:'flex', flexDirection:'row'}}>
                     <Grid container sx={{width:'48%',height:'36px', borderRadius:'3px', backgroundColor:'#FFF',margin:'3px auto',justifyContent:'center', alignContent:'baseline',paddingTop:'6px'}}>
                         <TrendingUpIcon color="success"/>
-                        <Typography sx={{textAlign:'center'}}>
+                        <Typography onClick={()=>handleMaximum} sx={{textAlign:'center',cursor:'pointer'}}>
                             بیشترین رشد
                         </Typography>
                     </Grid>
                     <Grid container sx={{width:'48%',height:'36px', borderRadius:'3px', backgroundColor:'#FFF',margin:'3px auto',justifyContent:'center', alignContent:'baseline',paddingTop:'6px'}}>
                         <TrendingDownIcon color="warning" />
-                        <Typography sx={{textAlign:'center',}}>
+                        <Typography OnClick={()=>handleMinimum} sx={{textAlign:'center',cursor:'pointer'}}>
                             کمترین رشد
                         </Typography>
                     </Grid>
                 </Grid>
             </Grid>
            
-            <TableContainer >
+            <TableContainer sx={{display:{ xs:'none' , md:'block'}}} >
                 <Table>
                     <TableHead>
-                        <TableRow sx={{backgroundColor:"#fafafa"}}>
+                        <TableRow sx={{backgroundColor:"#e0e0e0"}}>
                             <TableCell align="right">ارز دیجیتال</TableCell>
                             <TableCell align="right">قیمت خرید</TableCell>
                             <TableCell align="right">قیمت فروش</TableCell>
@@ -67,17 +82,39 @@ const MainTable=()=>{
                         </TableRow>
                     </TableHead>
                     <TableBody>
-                        {coins.map((coin,index)=>(
-                            <TableRow key={index}>
-                                <TableCell align="right">{coin.name}</TableCell>
-                                <TableCell align="right">{coin.current_price}</TableCell>
-                                <TableCell align="right">{coin.current_price}</TableCell>
-                                <TableCell align="right">{coin.price_change_percentage_24h}</TableCell>
-                            </TableRow>
-                        ))}
+                        {state === "maximum" ?
+                            maxValue.map((coin, index)=>(
+                                <TableRow key={index}>
+                                    <TableCell align="right">{coin.name}</TableCell>
+                                    <TableCell align="right">{coin.current_price}</TableCell>
+                                    <TableCell align="right">{coin.current_price}</TableCell>
+                                    <TableCell align="right" color="success">{coin.price_change_percentage_24h}</TableCell>
+                                </TableRow>
+                            ))
+                        :
+                            minValue.map((coin, index)=>(
+                                <TableRow key={index}>
+                                    <TableCell align="right">{coin.name}</TableCell>
+                                    <TableCell align="right">{coin.current_price}</TableCell>
+                                    <TableCell align="right">{coin.current_price}</TableCell>
+                                    <TableCell align="right" color="error">{coin.price_change_percentage_24h}</TableCell>
+                                </TableRow>
+                            ))
+                        }
                     </TableBody>
                 </Table>
             </TableContainer>
+            <Grid>
+                {state === "maximum" ?
+                    maxValue.map((coin, index)=>(
+                        <Card key={index} name={coin.name} image={coin.image} price={coin.current_price} change={coin.price_change_percentage_24h} color="success"/>
+                    ))
+                :
+                    minValue.map((coin, index)=>(
+                        <Card key={index} name={coin.name} image={coin.image} price={coin.current_price} change={coin.price_change_percentage_24h} color="error"/>
+                    ))
+                }
+            </Grid>
         </Grid>
     </div>
 )}
