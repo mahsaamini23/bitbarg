6dab1aba13e9ca78e3138adca19e3dc998bb33cc
ms.moradi23 <ms.moradi23@yahoo.com>
2022-11-08 23:02:36 +0330

    add theme

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import './App.css';
 // import Home from "./page/Home";
 import {RouterProvider} from "react-router-dom";
-import router from "./route/route";
+import Router from "./route/route";
 import {ThemeProvider} from "@mui/material/styles";
 import {theme} from "theme/theme";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <RouterProvider router={router}/>
+      <RouterProvider router={Router}/>
     </ThemeProvider>
   );
 }
