a0520b4fb82c99175b592e4f9f265f342da73bfe
ms.moradi23 <ms.moradi23@yahoo.com>
2022-11-08 23:15:55 +0330

    modify style

--- src/component/Layout/Layout.tsx
+++ src/component/Layout/Layout.tsx
@@ -1,16 +1,17 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
 
 
-
-const Layout =()=>{
+const Layout =():JSX.Element=>{
     return(
         <div>
             <Header/>
             <main>
                 <Outlet/>
             </main>
+            <Footer/>
         </div>
     )
 }
