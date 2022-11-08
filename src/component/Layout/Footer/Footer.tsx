3d97463d19dc2f0f8c2b334794ebe5f8f6e01ccc
ms.moradi23 <ms.moradi23@yahoo.com>
2022-11-08 23:16:19 +0330

    add footer

--- src/component/Layout/Footer/Footer.tsx
+++ src/component/Layout/Footer/Footer.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import Grid from "@mui/material/Grid";
+import Typography  from "@mui/material/Typography";
+import HomeIcon from '@mui/icons-material/Home';
+import { grey } from "@mui/material/colors";
 
 const Footer =()=>{
     return(
-        <Grid>
-
+        <Grid container  sx={{display:{xs:'flex' , md: 'none'} , justifyContent:'flexStart',width:"100%", position:'fixed', bottom:'0', borderTop:'1px solid grey'}}>
+            <Grid container item sx={{display:'flex' , flexDirection:'column', justifyContent:'space-evenly',width:'25%' , alignContent:'baseline',padding:'0 10px'}} >
+                <HomeIcon sx={{color : grey[500] , fontSize: 30}}/>
+                <Typography sx={{color:'grey'}}>
+                    خانه
+                </Typography>
+            </Grid>
         </Grid>
     )
 }
