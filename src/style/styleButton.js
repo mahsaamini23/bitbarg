0165be93b0a2e9d22f318b6fb7c9998f12fca00b
ms.moradi23 <ms.moradi23@yahoo.com>
2022-11-08 23:19:20 +0330

    add styled to button

---
+++ src/style/styleButton.js
@@ -0,0 +1,13 @@
+import {styled} from "@mui/material";
+import Button from "@mui/material/Button";
+
+
+const StyledButton = styled(Button)(({theme})=>({
+    // color:'grey',
+    // backgroundColor:'#f2f2f2',
+    color:'white',
+    backgroundColor:'#4285F2',
+    
+}))
+
+export default StyledButton;
